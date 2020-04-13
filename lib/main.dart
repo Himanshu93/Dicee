425b4bb37d912cf0980fc11bc5225561ded0aa85
Himanshu <himanshuchoudhary93@gmail.com>
2020-04-13 21:28:34 +0530

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,31 +17,54 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNum = 1;
+  var rightDiceNum = 1;
+  
   @override
   Widget build(BuildContext context) {
+
     return Center(
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  rollDice();
+                });
+                build(context);
+              },
               child: Image.asset(
-                'images/dice1.png',
+                'images/dice$leftDiceNum.png',
               ),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  rollDice();
+                });
+              },
               child: Image.asset(
-                'images/dice1.png',
+                'images/dice$rightDiceNum.png',
               ),
             ),
           ),
         ],
       ),
     );
+  }
+
+  void rollDice() {
+    leftDiceNum = Random().nextInt(6)+1;
+    rightDiceNum = Random().nextInt(6)+1;
   }
 }
